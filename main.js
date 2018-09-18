1511a24bf21736e72809d4b2d4fa7abb111585c8
Juha Kangas <juhakangas5@gmail.com>
2018-09-18 16:19:54 +0300

    added phrase converter

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var fs = require('fs');
 
 var data= fs.readFileSync('./dataBase/wordData.json');
@@ -25,5 +26,29 @@ console.log(noun.length);
 // +" "+noun[Math.floor(Math.random()*noun.length)].word
 // +" "+adverb[Math.floor(Math.random()*adverb.length)].word);
 
+// Word to type of word
 
+let phrase="The character's popularity has seen him featured in numerous forms of other media. In the 2004 series Cable & Deadpool, he refers to his own scarred appearance as"
 
+phrase=phrase.split(" ");
+let typeOfWord=[];
+
+for(var i=0;i<phrase.length;i++){
+    word=phrase[i].toLowerCase().replace(/[^a-zA-Z]/gi,"").split("").join("");
+    let flag=0;
+    typeOfWord.push(word);
+    for(var j=0;j<wordData.length;j++){
+        if(wordData[j].word===word){
+            if(typeOfWord.includes(wordData[j].type)==false){
+                typeOfWord.push('('+wordData[j].type+')')
+                console.log(wordData[j].type);
+                flag=1;
+            }     
+        }
+    }
+    if(flag==0){
+        typeOfWord.push("(unknown)");
+    }
+}
+// console.log(phrase);
+console.log(typeOfWord.join(" "));
